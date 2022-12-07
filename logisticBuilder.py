55adcd80e41b328f2dc2a30525da86ccba0b74bd
David Kristi <davidkristi07@gmail.com>
2022-12-07 15:25:26 +0700

    Updated file and new picture for rail

--- logisticBuilder.py
+++ logisticBuilder.py
@@ -14,7 +14,7 @@ speed = 200
 surface = pygame.display.set_mode((displayWidth, displayHeight))
 
 pygame.display.set_caption("Image")
-displayImage = pygame.image.load(".\game\demoGreen.jpg") 
+straightRail = pygame.image.load("game\Rail Road Straight-1.png")
 
 time0 = time.time()
 time1 = 0
@@ -44,20 +44,47 @@ class keyboard():
         horizontal = horizontal*dt
         return (horizontal, vertical)
 
+class entityElement():
+    
+    def __init__(self, typeNumber, position):
+        self.typeNumber = typeNumber
+        self.position = position
+
+class entityController():
+
+    def __init__(self):
+        self.list = []
+
+    def append(self,object):
+        if type(object) is entityElement:
+            self.list.append(object)
+
+    def getList(self):
+        return self.list
+
+dictionary = [straightRail]
 keyboard = keyboard()
+entity = entityController()
 
 while True : 
   
-    surface.fill((255,255,255))   
+    surface.fill((255,255,255))  
+
+    for eachEntity in entity.getList():
+        surface.blit(dictionary[eachEntity.typeNumber], eachEntity.position)
+         
     pygame.draw.rect(surface, (0,0,0), (rectanglePosition[0], rectanglePosition[1], rectangleSize[0], rectangleSize[1]))
   
     for event in pygame.event.get() : 
    
         if event.type == pygame.QUIT : 
-  
-   
             pygame.quit()  
             quit() 
+        elif event.type == pygame.KEYDOWN:
+            if pygame.key.get_pressed()[pygame.K_x]:
+                entity.append(entityElement(0, (rectanglePosition[0]+rectangleSize[0], rectanglePosition[1]+rectangleSize[1])))
+    
+    
     
     movement = keyboard.getMovement()
     rectanglePosition[0] += movement[0]
