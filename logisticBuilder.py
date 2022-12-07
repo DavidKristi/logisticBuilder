f4449bd32952cfbe564b9090fb4a6817dc61b742
David Kristi <davidkristi07@gmail.com>
2022-12-07 13:35:11 +0700

    file awal

---
+++ logisticBuilder.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+displayWidth = 800
+displayHeight = 600
+
+surface = pygame.display.set_mode((displayWidth, displayHeight))
+
+pygame.display.set_caption("Image")
+
